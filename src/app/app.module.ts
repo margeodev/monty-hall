1c6988cd32b2740db359b121e734509df0cb6f93
marcio <margeodev@gmail.com>
2019-07-01 13:16:39 -0300

    Alteração do css global, criação do component form-inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormInicialComponent } from './form-inicial/form-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormInicialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-inicial/form-inicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inicial',
+  templateUrl: './form-inicial.component.html',
+  styleUrls: ['./form-inicial.component.css']
+})
+export class FormInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
